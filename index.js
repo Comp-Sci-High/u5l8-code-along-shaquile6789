28f06bfaade22468efdb84ed4594dfc2c7d7e5d8
shaquile6789 <shaquille.oemrawsingh26@compscihigh.org>
2026-03-31 19:17:59 +0000

    done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 app.set("view engine", "ejs");
 
 app.use(express.json());
-
+app.use(express.static(__dirname + '/public'))
 const countrySchema = new mongoose.Schema({
   country: { type: String },
   flagURL: { type: String },
@@ -27,21 +27,46 @@ const Country = mongoose.model("Country", countrySchema, "Countries");
 // Create a POST route for "/add/country" that adds a country using the request body (3 points)
 // Use postman to add at least THREE different countries
 
+app.post('/add/country',async (req, res) =>{
+  const newCountry = await new Country({
+    country: req.body.country,
+    flagURL: req.body.flagURL,
+  population: req.body.population,
+  officialLanguage: req.body.officialLanguage,
+  hasNuclearWeapons: req.body.hasNuclearWeapons,
+  }).save()
 
+  res.json(newCountry)
+})
 // Create a GET route for "/" that renders countries.ejs with every country from the Countries collection (1 point)
-
+app.get('/', async (req, res) =>{
+  const countries = await Country.find({})
+  res.render('countries.ejs', {countries})
+})
 
 // Go to countries.ejs and follow the tasks there (2 points)
 
 
 // Create a dynamic PATCH route handler for "/update/{name}" that modifies the population of the country specified in the path (3 points)
 // Test this route on post man
-
+app.patch('update/:name', async (req, res) =>{
+  const updatedCountry = await Country.findOneAndUpdate(
+    {country: req.params.name}, 
+    {population: req.body.population}, 
+    {new: true}
+  )
+  res.json(updatedCountry)
+})
 
 
 // Create a DELETE route handler for "/delete/country" that deletes a country of your choice (3 points)
 // Test this route on post man
-
+app.delete('/delete/:name', async (req, res) =>{
+  const deletedCountry = await Country.findOneAndDelete(
+    {country: req.params.name}
+  )
+  res.json(deletedCountry)
+})
 
 async function startServer() {
   
